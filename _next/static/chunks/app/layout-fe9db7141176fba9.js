1bc502e8b126f9c5de5f0f34aa67fdbd53ff28b3
KapilDuwadi <kapil.duwadi34@gmail.com>
2023-09-10 09:15:00 -0600

    Updates

---
+++ _next/static/chunks/app/layout-fe9db7141176fba9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9057:function(e,n,t){Promise.resolve().then(t.t.bind(t,6685,23)),Promise.resolve().then(t.t.bind(t,2941,23)),Promise.resolve().then(t.t.bind(t,3177,23))},3177:function(){}},function(e){e.O(0,[237,971,596,744],function(){return e(e.s=9057)}),_N_E=e.O()}]);
